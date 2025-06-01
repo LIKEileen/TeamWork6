dbf823674b0065e85cc36d489df0029576a06a99
ArdentLiby <81418841+ArdentLiby@users.noreply.github.com>
2025-06-01 19:52:41 +0800

    Create App.vue

---
+++ openSourceFront/src/App.vue
@@ -0,0 +1,35 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+  import { watchEffect } from 'vue'
+  import { useUserStore } from '@/store/user'
+
+  const userStore = useUserStore()
+
+  watchEffect(() => {
+    document.body.setAttribute('data-theme', userStore.theme || 'light')
+})
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
